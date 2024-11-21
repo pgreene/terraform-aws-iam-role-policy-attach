6be1e9f993090431320ee6393e7830cc8d87108b
pgreene <pgreene@protonmail.com>
2024-11-21 11:52:41 -0500

    minor update

--- iam-role-policy.tf
+++ iam-role-policy.tf
@@ -1,8 +1,5 @@
 resource "aws_iam_role_policy_attachment" "general" {
-  //role = aws_iam_role.iam_for_lambda.name
-  //policy_arn = aws_iam_policy.lambda_policy.arn
   role = var.role_name
   policy_arn = var.policy_arn
-  //count = var.policy_yesno
 }
 
